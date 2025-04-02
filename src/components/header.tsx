247f361edfbe939302ecbafcc5dcfb383c42a9df
bhushan <pbhushan0603@gmail.com>
2025-04-02 10:43:50 +0530

    added about page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,23 +6,25 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 flex h-16 w-full items-center bg-opacity-50 px-4 text-white backdrop-blur-sm">
-      <div>
-        <Image
-          src={"/favicon.png"}
-          alt={"logo"}
-          width={40}
-          height={40}
-          className="mr-5 rounded-2xl"
-        />
-      </div>
-      <div className="font-semibold">ABT | OPEN SOURCE</div>
+      <Link href={"/"} className="flex items-center">
+        <div>
+          <Image
+            src={"/favicon.png"}
+            alt={"logo"}
+            width={40}
+            height={40}
+            className="mr-5 rounded-2xl"
+          />
+        </div>
+        <div className="font-semibold">ABT | OPEN SOURCE</div>
+      </Link>
       <div className="ml-auto flex items-center gap-2 md:gap-4">
-        <Link href={"/"}>
+        <Link href={"https://github.com/abt-opensource"}>
           <Button className="max-sm:hidden">
             <Github /> Explore
           </Button>
         </Link>
-        <Link href={"/"}>
+        <Link href={"/about-us"}>
           <Button variant={"secondary"} className="max-md:hidden">
             About us
           </Button>
